e68134ab3d73823d0c2f6ff7e8421fbaecd86df5
javiDocenteInformatica <javi.docente.informatica@gmail.com>
2022-11-22 18:39:30 +0100

    AJAX1 update
    Se añade:
    if(this.readyState == 4 && this.status == 200)

--- AJAX1/js/ajax.js
+++ AJAX1/js/ajax.js
@@ -5,12 +5,16 @@ window.onload = (evento) => {
   });
 };
 
+// Ver Libro página 148: https://sintesis.read.garden/viewer/desarrollo-web-en-entorno-cliente-2-edicion-2/148
 let f_loadDoc = () => {
   const xhttp = new XMLHttpRequest();
-  xhttp.onload = function () {
-    let objeto = JSON.parse(this.responseText);
-
-    document.getElementById("demo").innerHTML += f_objeto_toString(objeto);
+  xhttp.onreadystatechange = function () {
+    if(this.readyState == 4 && this.status == 200){
+      let objeto = JSON.parse(this.responseText);
+      document.getElementById("demo").innerHTML += f_objeto_toString(objeto);
+    }else{
+      console.log(` xhttp.readyState: ${this.readyState}`);
+    }    
   };
   xhttp.open("GET", "json/datos.json");
   xhttp.send();
