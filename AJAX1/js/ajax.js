ed54b3ab69d682ce7f0395569754cea5ad080b03
javiDocenteInformatica <javi.docente.informatica@gmail.com>
2022-11-28 09:45:45 +0100

    28/11/2022 - 9:45h

--- AJAX1/js/ajax.js
+++ AJAX1/js/ajax.js
@@ -7,8 +7,22 @@ let f_cargaDocumento; // función para cargar el documento en HTML
 window.addEventListener("load", ()=>{
     id_inputFichero = document.querySelector("#id_inputFichero");
     btn_obtieneFichero = document.querySelector("#btn_obtieneFichero");
+    // id_inputFichero.files[0].name="json\\datos.json";
+    
+    
+    btn_obtieneFichero.addEventListener("click", ()=>{
+        f_cargaDocumento();
+    });
+    
+    
+    
     // MOSTRAR POR CONSOLA
-    console.log(id_inputFichero.value);
+    // Comprueba el fichero
+    id_inputFichero.addEventListener("change", ()=>{
+        console.log(id_inputFichero);
+        console.log(id_inputFichero.files[0]);
+    });
+    
     // console.log(btn_obtieneFichero);
 });
 
@@ -20,24 +34,47 @@ f_cargaDocumento = ()=>{
         switch(xmlHttp.readyState){
             case 0:
                 console.log("xmlHttp.readyState: ","0: solicitud no inicializada");
+                console.log("xmlHttp.status: ",xmlHttp.status);
                 break;
             case 1:
                 console.log("xmlHttp.readyState: ","1: conexión de servidor establecida");
+                console.log("xmlHttp.status: ",xmlHttp.status);
                 break;
             case 2:
                 console.log("xmlHttp.readyState: ","2: solicitud recibida");
+                console.log("xmlHttp.status: ",xmlHttp.status);
                 break;
             case 3:
                 console.log("xmlHttp.readyState: ","3: solicitud de procesamiento");
+                console.log("xmlHttp.status: ",xmlHttp.status);
                 break;
             case 4:
                 console.log("xmlHttp.readyState: ","4: solicitud finalizada y respuesta lista");
-                break
+                console.log("xmlHttp.status: ",xmlHttp.status);
+                if(xmlHttp.status == 200){
+                    btn_obtieneFichero.insertAdjacentHTML("afterend",`<p>${this.responseText}</p>`);
+                }
+                break;
             default:
                 console.log("xmlHttp.readyState: ","Error, el estado no se encuentra entre 0 y 4");
+                console.log("xmlHttp.status: ",xmlHttp.status);
                 break;
         }
     }
+
+    /*
+    Especifica la solicitud:
+    - método: el tipo de solicitud GET o POST
+    - url: la ubicación del archivo
+    - async: verdadero (asincrónico) o falso (sincrónico)
+    - usuario: nombre de usuario opcional 
+    - psw: contraseña opcional
+    */
+    xmlHttp.open("GET",`../AJAX1/json/${id_inputFichero.files[0].name}`, true); //xmlHttp.open(method,url,async,user,psw);
+
+    xmlHttp.send(); //Envía la solicitud al servidor. Utilizado para solicitudes GET
+
+    console.log("xmlHttp.getAllResponseHeaders()",xmlHttp.getAllResponseHeaders()); // Devuelve información de encabezado
     
 };
 
