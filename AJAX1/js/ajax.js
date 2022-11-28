0a9b44ddec3fd03b83c0cbf078949b916af58aee
javiDocenteInformatica <javi.docente.informatica@gmail.com>
2022-11-28 09:15:25 +0100

    agregado enlace

--- AJAX1/js/ajax.js
+++ AJAX1/js/ajax.js
@@ -8,14 +8,36 @@ window.addEventListener("load", ()=>{
     id_inputFichero = document.querySelector("#id_inputFichero");
     btn_obtieneFichero = document.querySelector("#btn_obtieneFichero");
     // MOSTRAR POR CONSOLA
-    console.log(id_inputFichero);
-    console.log(btn_obtieneFichero);
+    console.log(id_inputFichero.value);
+    // console.log(btn_obtieneFichero);
 });
 
 
 f_cargaDocumento = ()=>{
     // Creamos el objeto para gestionar la petición AJAX 'XMLHttpRequest'
     let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = ()=>{
+        switch(xmlHttp.readyState){
+            case 0:
+                console.log("xmlHttp.readyState: ","0: solicitud no inicializada");
+                break;
+            case 1:
+                console.log("xmlHttp.readyState: ","1: conexión de servidor establecida");
+                break;
+            case 2:
+                console.log("xmlHttp.readyState: ","2: solicitud recibida");
+                break;
+            case 3:
+                console.log("xmlHttp.readyState: ","3: solicitud de procesamiento");
+                break;
+            case 4:
+                console.log("xmlHttp.readyState: ","4: solicitud finalizada y respuesta lista");
+                break
+            default:
+                console.log("xmlHttp.readyState: ","Error, el estado no se encuentra entre 0 y 4");
+                break;
+        }
+    }
     
 };
 
