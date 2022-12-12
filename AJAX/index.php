8888c9cbb0ae3c1339ae6179cadfcb7123fa0075
javiDocenteInformatica <javi.docente.informatica@gmail.com>
2022-12-12 07:24:38 +0100

    include favicon

--- AJAX/index.php
+++ AJAX/index.php
@@ -8,6 +8,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>AJAX1</title>
+
+  <!-- FAVICON -->
+  <link rel="icon" type="image/png" sizes="16x16" href="vista/img/cv_ico.png">
   
   <!-- ESTILOS CSS -->
   <!-- bootstrap CSS -->
